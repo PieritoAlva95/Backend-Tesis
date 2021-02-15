7e262951833c1fb4b6fc12fb53df88d1b62f414a
PieritoAlva95 <jpalvaradoc@unl.edu.ec>
2021-02-15 14:44:52 -0500

    Se agrego middleware
    Se agrego middleware para hacer las rutas de la API privadas

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function (req, res, next) {
+  // Obtener el token de la cabecera
+  const token = req.header('x-auth-token');
+
+  // Comprobar si no hay token
+  if (!token) {
+    return res.status(401).json({ msg: 'No hay token, autorización denegada' });
+  }
+
+  //Verificar el token
+  try {
+    jwt.verify(token, config.get('jwtSecret'), (error, decoded) => {
+      if (error) {
+        return res.status(401).json({ msg: 'El token no es válido' });
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } catch (error) {
+    console.error('Algo salio mal con el middleware de autentificación');
+    res.status(500).json({ msg: 'Error del servidor' });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+const User = require('../../models/User');
 
 // @route GET api/auth
-// @desc Ruta de prueba
-// @access Public
-router.get('/', (req, res) => res.send('Ruta de autorizacion'));
+// @desc Obtener usuario por token
+// @access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Error del servidor');
+  }
+});
 
 module.exports = router;
