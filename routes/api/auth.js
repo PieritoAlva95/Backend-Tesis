78cba3bb6c09fcf2487dfbad877148a73d6cb6de
PieritoAlva95 <jpalvaradoc@unl.edu.ec>
2021-02-15 15:16:56 -0500

    Se agrego el login de usuarios
    Se agrego la logica para que los ususarios puedan ingresar a la plataforma y obtengan su token

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
@@ -16,5 +20,59 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Error del servidor');
   }
 });
+
+// @route POST api/auth
+// @desc Autenticar al usuario y obtener el token
+// @access Public
+router.post(
+  '/',
+  check('email', 'Por favor, incluya un correo electrónico válido').isEmail(),
+  check('password', 'Se requiere una contraseña').exists(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Credenciales inválidas' }] });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Credenciales inválidas' }] });
+      }
+
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '5 days' },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Error del servidor');
+    }
+  }
+);
 
 module.exports = router;
