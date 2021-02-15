a6ec27d364a91548fad70baabe6c2cf3cdd4b40f
PieritoAlva95 <jpalvaradoc@unl.edu.ec>
2021-02-15 11:33:34 -0500

    Conexión a la base de datos
    Se agrego los archivos y la codificacion necesaria para la conexion de la base de datos con el backend

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/', (req, res)=> res.send('API corriendo'));
+// Conectar la base de datos
+connectDB();
+
+app.get('/', (req, res) => res.send('API corriendo'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=> console.log(`El servidor inicio en el puerto ${PORT}`));
+app.listen(PORT, () => console.log(`El servidor inicio en el puerto ${PORT}`));
