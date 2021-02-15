c5012e359ca79855d089056c71cab2fc80c902b9
PieritoAlva95 <jpalvaradoc@unl.edu.ec>
2021-02-15 16:52:11 -0500

    Se creo la ruta para obtener el perfil de usuario
    Se creo la logica para obtener el perfil del usuario actual

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route GET api/profile
-// @desc Ruta de prueba
-// @access Public
-router.get('/', (req, res) => res.send('Ruta de perfiles'));
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route GET api/profile/me
+// @desc Obtener el perfil de los usuarios actuales
+// @access Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'No hay perfil para este usuario' });
+    }
+
+    res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Error del servidor');
+  }
+});
 
 module.exports = router;
