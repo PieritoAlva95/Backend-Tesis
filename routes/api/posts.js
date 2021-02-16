2021d2010d096360a9b720e4022e96e28f42c6aa
PieritoAlva95 <jpalvaradoc@unl.edu.ec>
2021-02-16 10:56:01 -0500

    Se agrego rutas para los posts
    Se agrego las rutas para obtener, eliminar los posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
+const checkObjectId = require('../../middleware/checkObjectId');
 
 const Post = require('../../models/Post');
 const User = require('../../models/User');
@@ -37,5 +38,61 @@ router.post(
     }
   }
 );
+
+// @route GET api/posts
+// @desc Obtener todos los Posts
+// @access Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Error del servidor');
+  }
+});
+
+// @route GET api/posts/:id
+// @desc Obtener el post por ID
+// @access Private
+router.get('/:id', auth, checkObjectId('id'), async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post no encontrado' });
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Error del servidor');
+  }
+});
+
+// @route DELETE api/posts/:id
+// @desc Borrar un Post por el ID
+// @access   Private
+router.delete('/:id', [auth, checkObjectId('id')], async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post) {
+      return res.status(404).json({ msg: 'Post no encontrado' });
+    }
+
+    // Comprobar usuario
+    if (post.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Usuario no autorizado' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post eliminado' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Error del servidor');
+  }
+});
 
 module.exports = router;
