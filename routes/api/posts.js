60e51c251ef6ab55bd082d0bc4ebbb98d7e0e867
PieritoAlva95 <jpalvaradoc@unl.edu.ec>
2021-02-15 22:24:41 -0500

    Creacion de post
    Se agrego la ruta para crear un posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const auth = require('../../middleware/auth');
 
-// @route GET api/posts
-// @desc Ruta de prueba
-// @access Public
-router.get('/', (req, res) => res.send('Ruta de posts'));
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+
+// @route POST api/posts
+// @desc Crear un post
+// @access Private
+router.post(
+  '/',
+  auth,
+  check('text', 'El texto es obligatorio').notEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save();
+
+      res.json(post);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send('Error del servidor');
+    }
+  }
+);
 
 module.exports = router;
