7c196e03afc7812d434fc98b67233616d112d926
PieritoAlva95 <jpalvaradoc@unl.edu.ec>
2021-02-22 20:12:19 -0500

    Rutas para comentarios
    Se agrego las rutas para poder crear y eliminar comentarios de los post en la plataforma

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -146,4 +146,75 @@ router.put('/unlike/:id', auth, checkObjectId('id'), async (req, res) => {
   }
 });
 
+// @route POST api/posts/comment/:id
+// @desc Comentar un post
+// @access Private
+router.post(
+  '/comment/:id',
+  auth,
+  checkObjectId('id'),
+  check('text', 'El texto es obligatorio').notEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id);
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        user: req.user.id,
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save();
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Error del servidor');
+    }
+  }
+);
+
+// @route DELETE api/posts/comment/:id/:comment_id
+// @desc Eliminar un comentario
+// @access Private
+router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Extraer el comentario
+    const comment = post.comments.find(
+      (comment) => comment.id === req.params.comment_id
+    );
+
+    // Asegurarse de que el comentario existe
+    if (!comment) {
+      return res.status(404).json({ msg: 'El comentario no existe' });
+    }
+
+    // Comprobar usuario
+    if (comment.user.toString() !== req.user.id) {
+      return res.status(401).json({ msg: 'Usuario no autorizado' });
+    }
+
+    post.comments = post.comments.filter(
+      ({ id }) => id !== req.params.comment_id
+    );
+
+    await post.save();
+
+    return res.json(post.comments);
+  } catch (err) {
+    console.error(err.message);
+    return res.status(500).send('Error del servidor');
+  }
+});
+
 module.exports = router;
